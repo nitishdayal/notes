f89119ec1fe3afea46a005e4c782ce1eb20b6c7d
Nitish Dayal <nitishdayal@outlook.com>
2016-11-25 02:43:45 -0800

    angular 2 notes + official tutorial

---
+++ angular2/tutorial_tour_of_heroes/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+template: `
+   <h1>{{title}}</h1>
+   <a routerLink="/heroes">Heroes</a>
+   <router-outlet></router-outlet>
+ `
+
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
---
+++ angular2/tutorial_tour_of_heroes/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{
+      path: 'heroes',
+      component: HeroesComponent
+    }])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+}
